33cc810a82e7ffc374179cf774cd88fe6cbd479b
Maguas Tupaguas <mauricioaznar94@gmail.com>
2021-03-17 11:23:08 -0600

    first commit

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express from 'express'
+import {json} from "body-parser";
+
+const app = express()
+
+app.use(json())
+
+app.get('/', (req, res) => {
+    res.send('Hello world!')
+})
+
+app.listen(3000, () => {
+    console.log('Listening on port 3000')
+})
+
