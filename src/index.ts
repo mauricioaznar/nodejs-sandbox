cf31dde2c461d69320b34be1ea40be75c0621828
Maguas Tupaguas <mauricioaznar94@gmail.com>
2021-03-17 12:35:51 -0600

    Heroku

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,9 @@ const start = async () => {
         console.error(e)
     }
 
-    app.listen(3000, () => {
+    const port = process.env.PORT || 3000
+
+    app.listen(port, () => {
         console.log('Listening on port 3000')
     })
 }
