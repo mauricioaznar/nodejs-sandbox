b6ff99df2bf3bdce67cfd6cf456cdc98915e8766
Maguas Tupaguas <mauricioaznar94@gmail.com>
2021-03-17 11:36:00 -0600

    Heroku

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
-import express from 'express'
+import express, {Request, Response} from 'express'
 import {json} from "body-parser";
 
 const app = express()
 
 app.use(json())
 
-app.get('/', (req, res) => {
+app.get('/', (req: Request, res: Response) => {
     res.send('Hello world!')
 })
+
+const port = process.env.PORT || 3000
 
 app.listen(3000, () => {
     console.log('Listening on port 3000')
